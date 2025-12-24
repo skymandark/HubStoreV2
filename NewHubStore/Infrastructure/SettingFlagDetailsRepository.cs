504f153e72e9f48b7454526e00a342ec1724ac94
Huda.maher <Huda.maher@pioneers-securities.com>
2025-12-24 13:01:45 +0200

    setting fixed location.🔧

---
+++ NewHubStore/Infrastructure/SettingFlagDetailsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Domin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class SettingFlagDetailsRepository
+    {
+        public SettingFlagDetailsRepository(/*AppDbContext context*/)
+        {
+            // _context = context;
+        }
+
+        public async Task AddAsync(Setting_flag_details detail)
+        {
+            // _context.Add(detail); await _context.SaveChangesAsync();
+        }
+    }
+}
